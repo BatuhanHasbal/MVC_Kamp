9d14da375b1676de062b952008a4c22e7f3b6f3d
Batuhan HASBAL <batuhsbl28@gmail.com>
2021-07-30 20:08:20 +0300

    Mimari kuruldu. Kategori bölümü tamamlandı

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //DataAccess teki IcategoryDal a ulaşmak için _categoryDal adında field oluşturduk.
+        ICategoryDal _categoryDal;
+
+        //_categoryDal a atama yapabilmek için constructer method oluşturuyoruz.
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categoryDal.Insert(category); //repository deki Insert metota bağladık.
+
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categoryDal.Get(x => x.CategoryID == id); // ID den gelen değere eşit mi değil mi ?
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryDal.List();
+        }
+
+
+    }
+}
---
+++ MvcKamp/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using System.Web.Mvc;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using FluentValidation.Results;
+
+namespace MvcKamp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+
+            //validation işlemi
+            CategoryValidatior categoryValidator = new CategoryValidatior();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
+    }
+}
